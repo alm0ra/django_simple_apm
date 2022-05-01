fec2f957ff0b5069fa353bb3b0aab68547f50c79
ali <ali.mrd318@gmail.com>
2022-05-01 11:53:44 +0000

    update: allow all host

--- django_simple_apm/settings.py
+++ django_simple_apm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w1l3%bn=-u^d71&_0bza2h_^sh3%&@axp*#s+7!pm^)z@&5e!p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
